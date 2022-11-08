06aec9bc3b570e96c5140a8776b213e7b4fc67aa
faria-r <fariacse11@gmail.com>
2022-11-08 12:43:55 +0600

    created a API for id based service

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
   
 
 // setting up middlewares
@@ -17,11 +18,31 @@ app.get('/',(req,res)=>{
 //connecting mongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wxeycza.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const ServiceCollection = client.db('cookingLuxury').collection('services')
   //creating a async function
 async function run(){
 try{
-
+//API to get all services
+app.get('/allservices',async(req,res)=>{
+  const query={};
+  const cursor = ServiceCollection.find(query);
+  const services = await cursor.toArray();
+  res.send(services)
+});
+//get api for limited number services
+app.get('/services',async(req,res)=>{
+  const query={};
+  const cursor = ServiceCollection.find(query).limit(3);
+  const services = await cursor.toArray();
+  res.send(services)
+});
+app.get('/details/:id',async(req,res)=>{
+  const id = req.params.id;
+  const query = {_id:ObjectId(id)};
+  const cursor = ServiceCollection.find(query);
+  const service = await cursor.toArray();
+  res.send(service)
+})
 }
 finally{
     
